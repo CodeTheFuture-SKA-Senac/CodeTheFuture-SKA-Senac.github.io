85beb66b2b2ca175b5d6b69e983f6794df72c8fa
Andressa Hensel Cardoso <andressa.cardoso@ska.com.br>
2025-09-25 19:29:43 -0300

    Refatorando código

--- production/src/app/app.routes.ts
+++ production/src/app/app.routes.ts
@@ -2,5 +2,3 @@ import { Routes } from '@angular/router';
 import { Terminal } from './components/terminal/terminal';
 
 export const routes: Routes = [{ path: '', component: Terminal }];
-
-
--- production/src/app/components/dialog-stop-production/dialog-stop-production.ts
+++ production/src/app/components/dialog-stop-production/dialog-stop-production.ts
@@ -11,6 +11,8 @@ import { StopProductionType } from '../../../../../common/class/StopProductionTy
 import { ProductionStatus } from '../../../../../common/class/ProductionStatus';
 import { ProductionStatus_e, ProductionStatusColor_e } from '../../../../../common/enum/Enum';
 
+import stopTypes from '../../../assets/files/production-stop-types.json';
+
 @Component({
   selector: 'app-dialog-stop-production',
   imports: [MatDialogModule, MatButtonModule, CommonModule],
@@ -18,53 +20,9 @@ import { ProductionStatus_e, ProductionStatusColor_e } from '../../../../../comm
   styleUrl: './dialog-stop-production.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
+
 export class DialogStopProduction {
-  stopTypes: StopProductionType[] = [{
-      label: 'Parada - Almoço',
-      value: 'LunchStop',
-      key: '1',
-      icon: 'TbApple',
-      image: 'TbApple',
-      time: 3,
-      description: 'Tempo de parada: 3 min'
-    },
-    {
-      label: 'Parada - Treinamento do time',
-      value: 'TrainingStop',
-      key: '2',
-      icon: 'TbMoodBoy',
-      image: 'TbMoodBoy',
-      time: 2,
-      description: 'Tempo de parada: 2 min'
-    },
-    {
-      label: 'Parada - Queda de energia',
-      value: 'PowerOutageStop',
-      key: '3',
-      icon: 'TbLampOff',
-      image: 'TbLampOff',
-      time: 1,
-      description: 'Tempo de parada: 1 min'
-    },
-    {
-      label: 'Parada - Troca de operador',
-      value: 'OperatorChangeStop',
-      key: '4',
-      icon: 'TbStatusChange',
-      image: 'TbStatusChange',
-      time: null,
-      description: 'Rotação de setor'
-    },
-    {
-      label: 'Parada - Manutenção',
-      value: 'MaintenanceStop',
-      key: '5',
-      icon: 'TbTool',
-      image: 'TbTool',
-      time: null,
-      description: 'Resolva o desafio'
-    } 
-  ];
+  stopTypes: StopProductionType[] = stopTypes;
 
   constructor(
     public dialogRef: MatDialogRef<DialogStopProduction>,
